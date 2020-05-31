7cb1783540e8a6c3e56194bfafbfc737528bd22d
Shudan Zheng <49221837+Shooderm@users.noreply.github.com>
2020-05-31 17:54:09 +1000

    Create vanilla_LSTM.py

---
+++ RNN-models/vanilla_LSTM.py
@@ -0,0 +1,14 @@
+class LSTM_model(nn.Module):
+    def __init__(self):
+        super(LSTM_model, self).__init__()
+        self.lstm = nn.LSTM(emb_size, hidden, batch_first=True, dropout=0.2)
+        self.dropout = nn.Dropout(0.2)
+        self.hidden = nn.Linear(hidden, hidden)
+        self.out  = nn.Linear(hidden, num_classes)
+
+    def forward(self, x):
+        lstm_out,(x,c_n) = self.lstm(x)
+        x = self.dropout(lstm_out[:,-1,:])
+        x = self.out(x)
+        x = F.log_softmax(x, dim=-1)
+        return x
